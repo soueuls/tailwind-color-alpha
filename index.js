22c096293e8cf00453593158df6ff24704c0e39a
Swann Polydor <swann.polydor@epitech.eu>
2020-03-07 00:01:14 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+module.exports = function() {
+  const hexToRgb = hex => {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return (
+      result && {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+      }
+    );
+  };
+
+  return function({ addUtilities, theme }) {
+    let colors = theme("colors", []);
+    let opacities = theme("opacity", []);
+    const newColors = {};
+
+    for (const [key, value] of Object.entries(colors)) {
+      const colorGroup = typeof value === "string" ? { [key]: value } : value;
+      for (const [colorName, colorValue] of Object.entries(colorGroup)) {
+        let rgb = hexToRgb(colorValue);
+        if (rgb) {
+          for (const o in opacities) {
+            const colorVariant =
+              typeof value === "string" || colorName === "default"
+                ? key
+                : `-${key}-${colorName}`;
+            newColors[`.bg-${colorVariant}-${o}`] = {
+              "background-color": `rgba(${rgb.r},${rgb.g},${rgb.b}, ${opacities[o]})`
+            };
+          }
+        }
+      }
+    }
+    addUtilities(newColors);
+  };
+};
